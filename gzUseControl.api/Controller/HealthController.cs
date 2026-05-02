c33263fd5df8e9c05501b3aed0a3c77116c90fc0
Alex <Alex@NO_DEFENDER>
2026-05-02 20:44:02 +0300

    Add project files.

---
+++ gzUseControl.api/Controller/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gzUseControl.api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+    }
+}
