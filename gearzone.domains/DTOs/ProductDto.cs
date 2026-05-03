ad61c797cffea3274876ebc0a5f4053da5b2ac48
Alex <Alex@NO_DEFENDER>
2026-05-03 04:12:58 +0300

    добавил сущность Product и ProductDto

---
+++ gearzone.domains/DTOs/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿namespace gearzone.domains.DTOs;
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public decimal? OldPrice { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public int Rating { get; set; }
+    public int Reviews { get; set; }
+    public string? Badge { get; set; }
+    public bool InStock { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
